a8823ea5ddc66be3e539642c477bd940d5635032
Shawn Inder <shawninder@gmail.com>
2018-05-01 18:50:34 -0400

    Point to newer version of party-server

--- env-config.js
+++ env-config.js
@@ -6,6 +6,6 @@ const internalIP = ip.address()
 module.exports = {
   // TODO Get local network IP programmatically
   'process.env.YOUTUBE_SEARCH_URL': prod ? 'https://youtube-search-njnnryxzgn.now.sh' : `http://${internalIP}:3001`,
-  'process.env.WS_SERVER_URL': prod ? 'https://party-server-bcaqpmhosw.now.sh' : `http://${internalIP}:8000`,
+  'process.env.WS_SERVER_URL': prod ? 'https://party-server-mgbwhuvxgq.now.sh' : `http://${internalIP}:8000`,
   'process.env.INTERNAL_IP': internalIP
 }
