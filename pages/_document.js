1c2cbd6812401524625b8a4341afb28b857670ef
Shawn Inder <shawninder@gmail.com>
2018-05-11 23:19:26 -0400

    theme color

--- pages/_document.js
+++ pages/_document.js
@@ -18,7 +18,7 @@ export default class MyDocument extends Document {
           <link rel='shotcut icon' href='/static/favicon.ico' />
           <meta httpEquiv='Content-Language' content={lang} />
           <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
-          <meta name='theme-color' content='black' />
+          <meta name='theme-color' content='whitesmoke' />
           <link rel='manifest' href='/static/manifest.json' />
           <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css' />
           <link rel='stylesheet' href='/_next/static/style.css' />
