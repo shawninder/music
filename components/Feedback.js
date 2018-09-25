a5d64a1c59d8b487ae3c50f52a8ee9b20a8d02ee
Shawn Inder <shawninder@gmail.com>
2018-09-24 23:07:30 -0400

    Feedback for feedback form (closes #52)

--- components/Feedback.js
+++ components/Feedback.js
@@ -10,55 +10,63 @@ class Feedback extends Component {
     this.onSubmit = this.onSubmit.bind(this)
 
     this.state = {
-      type: 'feature request',
       body: '',
-      email: ''
+      email: '',
+      submitting: false,
+      submitted: false
     }
   }
   onSubmit (event) {
     event.stopPropagation()
     event.preventDefault()
-    this.props.dispatch({
-      type: 'Feedback:submitting'
+    this.setState({
+      submitting: true
     })
     fetch(`${process.env.API_URL}/feedback`, {
       method: 'post',
       body: JSON.stringify(this.state)
     }).then((response) => {
       if (response.ok) {
-        return response.json()
+        this.setState({
+          submitting: false,
+          submitted: true
+        })
       } else {
+        this.setState({
+          submitting: false,
+          submitted: false
+        })
         throw response
       }
-    }).then((data) => {
-      console.log('data', data)
     }).catch((ex) => {
       console.log('ex', ex)
     })
   }
   render () {
+    let classNames = []
+    let buttonTxt = 'Send'
+    if (this.state.submitting) {
+      classNames.push('submitting')
+      buttonTxt = 'Sending'
+    }
+    if (this.state.submitted) {
+      classNames.push('submitted')
+      buttonTxt = 'Sent'
+    }
     return (
       <div className='Feedback'>
-        <form onSubmit={this.onSubmit}>
-          <select className='feedback-type' onChange={(event) => {
-            this.setState({ type: event.target.value })
-          }}>
-            <option>feature request</option>
-            <option>bug report</option>
-            <option>question</option>
-            <option>other</option>
-          </select>
+        <form onSubmit={this.onSubmit} className={classNames.join(' ')}>
           <h2>
             {this.props.caption}
           </h2>
           <textarea onChange={(event) => {
-            this.setState({ body: event.target.value })
+            this.setState({ body: event.target.value, submitted: false })
           }} />
           <label>Your e-mail (optional)</label>
           <input type='email' placeholder='♫@♭mail♩com' onChange={(event) => {
-            this.setState({ email: event.target.value })
+            this.setState({ email: event.target.value, submitted: false })
           }} />
-          <button type='submit'>Send</button>
+          <button type='submit'>{buttonTxt}</button>
         </form>
       </div>
     )
