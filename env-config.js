8f531ed9c5be7b9a70238bb1ebfd3d61ec2e6b18
Shawn Inder <shawninder@gmail.com>
2018-10-06 23:43:51 -0400

    point to newer api and party-server

--- env-config.js
+++ env-config.js
@@ -4,7 +4,7 @@ const prod = process.env.NODE_ENV === 'production'
 const internalIP = ip.address()
 
 module.exports = {
-  'process.env.API_URL': prod ? 'https://api-bwyimwflbr.now.sh' : `http://${internalIP}:3002`,
-  'process.env.WS_SERVER_URL': prod ? 'https://party-server-dxokaqrnmo.now.sh' : `http://${internalIP}:8000`,
+  'process.env.API_URL': prod ? 'https://api-hoojsocmjh.now.sh' : `http://${internalIP}:3002`,
+  'process.env.WS_SERVER_URL': prod ? 'https://party-server-cmedpubxnf.now.sh' : `http://${internalIP}:8000`,
   'process.env.INTERNAL_IP': internalIP
 }
