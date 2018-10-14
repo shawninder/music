278d75a9a76f1cdcd1c96ca3975afb2b7227ee56
Shawn Inder <shawninder@gmail.com>
2018-10-14 18:31:40 -0400

    point to newer versions, style fix for next errors

--- env-config.js
+++ env-config.js
@@ -4,7 +4,7 @@ const prod = process.env.NODE_ENV === 'production'
 const internalIP = ip.address()
 
 module.exports = {
-  'process.env.API_URL': prod ? 'https://api-hoojsocmjh.now.sh' : `http://${internalIP}:3002`,
-  'process.env.WS_SERVER_URL': prod ? '?' : `http://${internalIP}:8000`,
+  'process.env.API_URL': prod ? 'https://api-yqlaxfretn.now.sh' : `http://${internalIP}:3002`,
+  'process.env.WS_SERVER_URL': prod ? 'https://hub-ieitzgujzs.now.sh' : `http://${internalIP}:8000`,
   'process.env.INTERNAL_IP': internalIP
 }
