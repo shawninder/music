39e972e1b1d639527752bda84d74fab8bb5d1351
Shawn Inder <shawninder@gmail.com>
2018-10-14 21:31:57 -0400

    removed annoying log

--- features/auth/reducer.js
+++ features/auth/reducer.js
@@ -1,7 +1,6 @@
 import cloneDeep from 'lodash.clonedeep'
 
 export default function authReducer (state = {}, action) {
-  console.log('REDUCER', action)
   let newState = cloneDeep(state)
   switch (action.type) {
     case 'Auth:setUsername':
