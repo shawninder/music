5c14bdfb14e9ed597522a229cbe39ac21ecf2fe1
Shawn Inder <shawninder@gmail.com>
2019-01-09 16:21:32 -0500

    Newer deployments of api and hub

--- env-config.js
+++ env-config.js
@@ -4,7 +4,7 @@ const prod = process.env.NODE_ENV === 'production'
 const internalIP = ip.address()
 
 module.exports = {
-  'process.env.API_URL': prod ? 'https://api-yqlaxfretn.now.sh' : `http://${internalIP}:3002`,
-  'process.env.WS_SERVER_URL': prod ? 'https://hub-ieitzgujzs.now.sh' : `http://${internalIP}:8000`,
+  'process.env.API_URL': prod ? 'https://api-inmuyqqxwz.now.sh' : `http://${internalIP}:3002`,
+  'process.env.WS_SERVER_URL': prod ? 'https://hub-lrdpryezbg.now.sh' : `http://${internalIP}:8000`,
   'process.env.INTERNAL_IP': internalIP
 }
