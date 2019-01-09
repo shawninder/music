8541bf6df298bc0524c17f2c75ea98dcbfb0c996
Shawn Inder <shawninder@gmail.com>
2019-01-09 16:21:58 -0500

    Use phase and remove manifest and SW

--- next.config.js
+++ next.config.js
@@ -1,26 +1,41 @@
+const { PHASE_EXPORT, PHASE_PRODUCTION_BUILD, PHASE_PRODUCTION_SERVER, PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 const compose = require('next-compose-plugins')
-const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
-const withManifest = require('next-manifest')
-const withSW = require('next-offline')
+// const withManifest = require('next-manifest')
+// const withSW = require('next-offline')
 const withBabelCacheBusting = require('./withBabelCacheBusting')
-const manifest = require('./manifest.js')
+// const manifest = require('./manifest.js')
 
-module.exports = compose([
-  [withManifest, { manifest: { manifest } }],
-  withBabelCacheBusting,
-  [withBundleAnalyzer, {
-    analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
-    analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
-    bundleAnalyzerConfig: {
-      server: {
-        analyzerMode: 'static',
-        reportFilename: '../../bundles/server.html'
-      },
-      browser: {
-        analyzerMode: 'static',
-        reportFilename: '../bundles/client.html'
+module.exports = (phase, { defaultConfig }) => {
+  const config = [
+    // [withManifest({ manifest })],
+    withBabelCacheBusting
+    // [withSW()]
+  ]
+  if (phase === PHASE_EXPORT) {
+
+  }
+  if (phase === PHASE_PRODUCTION_BUILD) {
+
+  }
+  if (phase === PHASE_PRODUCTION_SERVER) {
+
+  }
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
+    config.push([withBundleAnalyzer, {
+      analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+      analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+      bundleAnalyzerConfig: {
+        server: {
+          analyzerMode: 'static',
+          reportFilename: '../../bundles/server.html'
+        },
+        browser: {
+          analyzerMode: 'static',
+          reportFilename: '../bundles/client.html'
+        }
       }
-    }
-  }],
-  withSW
-])
+    }])
+  }
+  return compose(config)
+}
