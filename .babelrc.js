4337816399486b49e5ed6cb84084500116f760f7
Shawn Inder <shawninder@gmail.com>
2019-01-11 18:29:51 -0500

    Added autoprefixed

--- .babelrc.js
+++ .babelrc.js
@@ -1,7 +1,9 @@
 const envConfig = require('./env-config')
 module.exports = exports = {
   "presets": [
-    "next/babel"
+    ["next/babel", {
+      "styled-jsx": { "plugins": ["styled-jsx-plugin-postcss"] }
+    }]
   ],
   "plugins": [
     ["transform-define", envConfig]
--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,13 @@
+const get = require('lodash.get')
+
 module.exports = (ctx) => {
+  const originalOptions = get(ctx, 'options.autoprefixer')
   const plugins = {
     autoprefixer: {
-      browsers: [
-        '>1%',
-        'last 4 versions',
-        'Firefox ESR',
-        'not ie < 9'
-      ]
+      ...originalOptions,
+      grid: true
     }
   }
+  plugins['postcss-nested'] = {}
   return { plugins }
 }
