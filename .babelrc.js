11d9f4d3c5e8d2b77ca0341e0a8ca5e304cd87fd
Shawn Inder <shawninder@gmail.com>
2019-01-11 19:02:21 -0500

    Added babel/env

--- .babelrc.js
+++ .babelrc.js
@@ -3,7 +3,8 @@ module.exports = exports = {
   "presets": [
     ["next/babel", {
       "styled-jsx": { "plugins": ["styled-jsx-plugin-postcss"] }
-    }]
+    }],
+    "@babel/env"
   ],
   "plugins": [
     ["transform-define", envConfig]
