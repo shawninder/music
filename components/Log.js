92c4cdfbad5c741819c967e95b65cade7a3260db
Shawn Inder <shawninder@gmail.com>
2019-01-21 15:10:56 -0500

    improve logs slightly

--- components/Log.js
+++ components/Log.js
@@ -2,20 +2,37 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import defaultProps from '../helpers/defaultProps'
 import propTypes from '../helpers/propTypes'
+import formatDate from '../helpers/formatDate'
 
 class Log extends Component {
   render () {
     const classes = this.props.className ? this.props.className.split(' ') : []
     classes.push('log')
-    const { name, type, key, ...rest } = this.props.data
+    const { _id, name, type, key, msg, ...rest } = this.props.data
+    const timestamp = _id.toString().substring(0, 8)
+    const date = new Date(parseInt(timestamp, 16) * 1000)
     return (
       <div
         className={classes.join(' ')}
       >
-        <h3>{name}</h3>
+        <h3><span className='date'>{formatDate(date)}</span>{name}</h3>
+        {msg
+          ? <p className='message'>{msg.name || JSON.stringify(msg)}</p>
+          : null
+        }
         <pre>
           {JSON.stringify(rest, null, 2)}
         </pre>
+        <style jsx>{`
+          h3 {
+            font-weight: bold;
+          }
+          .date {
+            font-weight: normal;
+            font-style: italic;
+            margin-right: 1em;
+          }
+        `}</style>
       </div>
     )
   }
