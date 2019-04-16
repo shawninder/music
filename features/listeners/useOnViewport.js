4b2d09701969c36760920530598c729a79405d56
Shawn Inder <shawninder@gmail.com>
2019-04-16 13:58:48 -0400

    Use passive on event listeners

--- features/listeners/useOnViewport.js
+++ features/listeners/useOnViewport.js
@@ -1,5 +1,8 @@
 import { useEffect } from 'react'
 
+import passiveSupported from '../../helpers/passiveSupported'
+
+const listenerOptions = passiveSupported ? { passive: true, capture: false } : false
 const isServer = typeof window === 'undefined'
 
 export default function useOnViewport (cb) {
@@ -7,11 +10,11 @@ export default function useOnViewport (cb) {
     if (!isServer) {
       // see https://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport/7557433#7557433
       if (global.addEventListener) {
-        global.addEventListener('load', cb, false)
-        global.addEventListener('DOMContentLoaded', cb, false)
-        global.addEventListener('load', cb, false)
-        global.addEventListener('scroll', cb, false)
-        global.addEventListener('resize', cb, false)
+        global.addEventListener('load', cb, listenerOptions)
+        global.addEventListener('DOMContentLoaded', cb, listenerOptions)
+        global.addEventListener('load', cb, listenerOptions)
+        global.addEventListener('scroll', cb, listenerOptions)
+        global.addEventListener('resize', cb, listenerOptions)
       } else if (global.attachEvent) {
         global.attachEvent('onload', cb)
         global.attachEvent('onDOMContentLoaded', cb)
@@ -24,11 +27,11 @@ export default function useOnViewport (cb) {
       if (!isServer) {
         // see https://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport/7557433#7557433
         if (global.removeEventListener) {
-          global.removeEventListener('load', cb, false)
-          global.removeEventListener('DOMContentLoaded', cb, false)
-          global.removeEventListener('load', cb, false)
-          global.removeEventListener('scroll', cb, false)
-          global.removeEventListener('resize', cb, false)
+          global.removeEventListener('load', cb, listenerOptions)
+          global.removeEventListener('DOMContentLoaded', cb, listenerOptions)
+          global.removeEventListener('load', cb, listenerOptions)
+          global.removeEventListener('scroll', cb, listenerOptions)
+          global.removeEventListener('resize', cb, listenerOptions)
         } else if (global.detachEvent) {
           global.detachEvent('onload', cb)
           global.detachEvent('onDOMContentLoaded', cb)
